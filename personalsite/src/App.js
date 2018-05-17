006b8869f6cf0972c85e60abd87599006b42fa6e
Eric Stanulis <erstanulis@gmail.com>
2018-05-16 20:53:26 -0500

    First section up and running

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Section from './components/section';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Section />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
       </div>
     );
   }
